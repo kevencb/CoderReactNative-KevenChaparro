d5fe7dc6c066e7c9df0e64a9f6249078fd1dc2a9
Keven CB <kevcb369@gmail.com>
2023-02-11 15:52:33 -0500

    Incio de proyecto, primera entrega.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        ¡Hola Coder House {"\n"} soy Keven Chaparro!
+      </Text>
     </View>
   );
 }
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1A1A2E',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+
+  title: {
+    fontSize: '24px',
+    textAlign: 'center',
+    lineHeight: '32px',
+    color: '#E94560'
+  }
 });
